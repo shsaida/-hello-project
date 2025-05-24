0ee9f21dcbe40056f45fba5b4ffc04e2e42aebad
a <yaraketka@gmail.com>
2025-05-24 10:35:53 +0300

    Fixed the function

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 def greeting(name): 
-print(f"Good morning, {name}!") 
-name = input("Your name: ") 
-greeting(name) 
+    print(f"Good morning, {name}!")
+
+
+name = input("Your name: ")
+greeting(name)
